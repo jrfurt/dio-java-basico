7f9b9b79d6a35dde9670dfd5e8780aaaa3887d8b
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-05-29 16:17:28 -0300

    estudos sobre anatomia das classes, add diretório 02

---
+++ 02-java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class MinhaClasse {
+  public static void main(String[] args) {
+    System.out.println("Fala, galerinha do meu canal!");
+
+    String primeiroNome = "Clodomir";
+    String sobrenome = "Junior";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, sobrenome);
+    System.out.println(nomeCompleto);
+  }
+
+  public static String nomeCompleto(String primeiroNome, String sobrenome) {
+    return primeiroNome.concat(" ").concat(sobrenome);
+  }
+}
