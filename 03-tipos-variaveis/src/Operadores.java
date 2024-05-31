710d59a2a89b8d33572bb0ee9e9505106b0da29f
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-05-31 19:45:03 -0300

    aula sobre operadores

---
+++ 03-tipos-variaveis/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+  public static void main(String[] args) {
+    int numero = 13;
+    System.out.println("positivo: " + numero);
+
+    numero = -numero;
+    System.out.println("negativo: " + numero);
+
+    numero = -numero;
+    System.out.println("positivo: " + numero);
+  }
+}
