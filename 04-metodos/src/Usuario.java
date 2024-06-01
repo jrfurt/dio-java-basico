c320b232328f99572599ffa9f73e5aead36d6844
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-05-31 21:06:57 -0300

    crinado métodos para aumentar volume e diminuir

--- 04-metodos/src/Usuario.java
+++ 04-metodos/src/Usuario.java
@@ -1,5 +1,5 @@
 public class Usuario {
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     SmartTv smartTv = new SmartTv();
     statusInicialSmartTv(smartTv);
 
@@ -7,13 +7,22 @@ public class Usuario {
     System.out.println("TV ligada? " + smartTv.ligada);
     smartTv.desligar();
     System.out.println("TV ligada? " + smartTv.ligada);
+    smartTv.aumentarVolume();
+    System.out.println("Volume: " + smartTv.volume);
+    smartTv.diminuirVolume();
+    System.out.println("Volume: " + smartTv.volume);
+    smartTv.passarCanal();
+    System.out.println("Canal: " + smartTv.canal);
+    smartTv.voltarCanal();
+    System.out.println("Canal: " + smartTv.canal);
   }
 
   public static void statusInicialSmartTv(SmartTv smartTv) {
-    System.out.println("|---------------------|");
-    System.out.println("  TV ligada? " + smartTv.ligada);
-    System.out.println("  Canal: " + smartTv.canal);
-    System.out.println("  Volume: " + smartTv.volume);
-    System.out.println("|---------------------|");
+    System.out.println("----------------");
+    System.out.println("*Status Inicial*");
+    System.out.println("TV ligada? " + smartTv.ligada);
+    System.out.println("Canal: " + smartTv.canal);
+    System.out.println("Volume: " + smartTv.volume);
+    System.out.println("----------------");
   }
 }
