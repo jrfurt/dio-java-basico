acd8dfe8be1982111ae4bdd0f475b9dd95742e35
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-08-02 08:26:03 -0300

    criacao de métodos de pesquisa com List

--- Aulas de Java/src/com/estudos/aula8/collections/list/pesquisa/CatalogoLivros.java
+++ Aulas de Java/src/com/estudos/aula8/collections/list/pesquisa/CatalogoLivros.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 public class CatalogoLivros {
 	List<Livro> livroList;
-	
+
 	public CatalogoLivros() {
 		livroList = new ArrayList<>();
 	}
-	
+
 	public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
 		livroList.add(new Livro(titulo, autor, anoPublicacao));
 	}
-	
+
 	public List<Livro> pesquisarPorAutor(String autor) {
 		List<Livro> livrosPorAutor = new ArrayList<>();
-		
+
 		if (!livroList.isEmpty()) {
 			for (Livro l : livroList) {
 				if (l.getAutor().equalsIgnoreCase(autor)) {
@@ -24,17 +24,48 @@ public class CatalogoLivros {
 				}
 			}
 		}
-		
+
 		return livrosPorAutor;
 	}
-	
+
+	public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
+		List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+
+		if (!livroList.isEmpty()) {
+			for (Livro l : livroList) {
+				if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+					livrosPorIntervaloAnos.add(l);
+				}
+			}
+		}
+
+		return livrosPorIntervaloAnos;
+	}
+
+	public Livro pesquisarPorTitulo(String titulo) {
+		Livro livroPorTitulo = null;
+
+		if (!livroList.isEmpty()) {
+			for (Livro l : livroList) {
+				if (l.getTitulo().equalsIgnoreCase(titulo)) {
+					livroPorTitulo = l;
+					break;
+				}
+			}
+		}
+
+		return livroPorTitulo;
+	}
+
 	public static void main(String[] args) {
 		CatalogoLivros catalogo = new CatalogoLivros();
-		
+
 		catalogo.adicionarLivro("Julio Cesar", "Julio", 2016);
 		catalogo.adicionarLivro("Meus afazeres", "Julio", 2018);
 		catalogo.adicionarLivro("Joao e Jose", "Joao", 2015);
 		catalogo.adicionarLivro("Juristinga", "Reizin", 2013);
 		System.out.println(catalogo.pesquisarPorAutor("Julio"));
+		System.out.println(catalogo.pesquisarPorIntervaloAnos(2013, 2015));
+		System.out.println(catalogo.pesquisarPorTitulo("meus afazeres"));
 	}
 }
