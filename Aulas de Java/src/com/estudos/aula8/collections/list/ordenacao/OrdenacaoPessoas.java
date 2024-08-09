31ea7a2866248ac6076543b20dd2789fc9f4a070
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-08-09 08:33:47 -0300

    implementação dos métodos utilizando Comparable e Comparator

--- Aulas de Java/src/com/estudos/aula8/collections/list/ordenacao/OrdenacaoPessoas.java
+++ Aulas de Java/src/com/estudos/aula8/collections/list/ordenacao/OrdenacaoPessoas.java
@@ -1,5 +1,46 @@
 package com.estudos.aula8.collections.list.ordenacao;
 
-public class OrdenacaoPessoas {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class OrdenacaoPessoas {
+	List<Pessoa> listPessoas;
+	
+	public OrdenacaoPessoas() {
+		listPessoas = new ArrayList<>();
+	}
+	
+	public void adicionarPessoa(String nome, int idade, double altura) {
+		listPessoas.add(new Pessoa(nome, idade, altura));
+	}
+	
+	public List<Pessoa> ordenarPorIdade() {
+		List<Pessoa> pessoasPorIdade = new ArrayList<>(listPessoas);
+		Collections.sort(pessoasPorIdade);
+		return pessoasPorIdade;
+	}
+	
+	public List<Pessoa> ordenarPorAltura() {
+		List<Pessoa> pessoasPorAltura = new ArrayList<>(listPessoas);
+		Collections.sort(pessoasPorAltura, new ComparatorAltura());
+		return pessoasPorAltura;
+	}
+	
+	public void exibirPessoas() {
+		System.out.println(listPessoas);
+	}
+	
+	public static void main(String[] args) {
+		OrdenacaoPessoas ordenacao = new OrdenacaoPessoas();
+		
+		ordenacao.adicionarPessoa("Junior", 18, 1.87);
+		ordenacao.adicionarPessoa("Vanderlan", 56, 1.71);
+		ordenacao.adicionarPessoa("Conde Drácula", 120, 1.85);
+		ordenacao.adicionarPessoa("Armando", 25, 1.68);
+	
+		ordenacao.exibirPessoas();
+		System.out.println(ordenacao.ordenarPorIdade());
+		System.out.println(ordenacao.ordenarPorAltura());
+	}
 }
--- Aulas de Java/src/com/estudos/aula8/collections/list/ordenacao/Pessoa.java
+++ Aulas de Java/src/com/estudos/aula8/collections/list/ordenacao/Pessoa.java
@@ -1,6 +1,7 @@
 package com.estudos.aula8.collections.list.ordenacao;
 
-public class Pessoa {
+// Comparable e sempre implementado na propria classe
+public class Pessoa implements Comparable<Pessoa> {
 	private String nome;
 	private int idade;
 	private double altura;
@@ -26,8 +27,13 @@ public class Pessoa {
 	@Override
 	public String toString() {
 		return "{ nome: " + nome
-			+ "idade: " + idade 
-			+ "altura: " + altura
+			+ ", idade: " + idade 
+			+ ", altura: " + altura
 			+ " }";
+	}
+
+	@Override
+	public int compareTo(Pessoa p) {
+		return Integer.compare(idade, p.getIdade());
 	}
 }
